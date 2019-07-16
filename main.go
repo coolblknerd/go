d4901871e687d45844c24c7f699c76c46b33b337
Reggie Davis <reginald.davis88@gmail.com>
2019-07-16 00:10:12 -0500

    Added example of mutex

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
+
+var (
+	mutex   sync.Mutex
+	balance int
+)
+
+func init() {
+	balance = 1000
+}
+
+func deposit(value int, done chan bool) {
+	mutex.Lock()
+	fmt.Printf("Depositing %d to account with balance: %d\n", value, balance)
+	balance += value
+	mutex.Unlock()
+	done <- true
+}
+
+func withdraw(value int, done chan bool) {
+	mutex.Lock()
+	fmt.Printf("Withdrawing %d from account with balance: %d\n", value, balance)
+	balance -= value
+	mutex.Unlock()
+	done <- true
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println("Go Mutex Example")
+
+	done := make(chan bool)
+	go withdraw(700, done)
+	go deposit(500, done)
+	<-done
+	<-done
+
+	fmt.Printf("New Balance %d\n", balance)
 }
